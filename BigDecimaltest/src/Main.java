662f1b86ad01c1863aca10257cf8a4bf207f9beb
erickvaernet-globant <erickadrielnet@gmail.com>
2022-12-28 14:05:39 -0300

    Revert "asdas"
    This reverts commit 15a32fc7d194b979fcbd027a8c93622905da16e1.

--- BigDecimaltest/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
-public class Main {
-    public static void main(String[] args) {
-        BigDecimal bg = new BigDecimal(10);
-        System.out.println(bg.divide(new BigDecimal(3),3 ,RoundingMode.CEILING));
-    }
-}
